682d2e41c25da3a15ea97abfa62a4edb660dc9f2
Artemusss <matema05@yandex.ru>
2024-09-30 20:00:47 +0300

    fixed creation

--- task 1 (LFU cache)/include/cache.hpp
+++ task 1 (LFU cache)/include/cache.hpp
@@ -7,21 +7,15 @@
 template <typename T> class LFU {
     struct freq_node;
     
+    using freq_iterator = typename std::list<freq_node>::iterator;
     struct node {
         T val;
-        typename std::list<freq_node>::iterator head;
-
-        public:
-        node(T inp, typename std::list<freq_node>::iterator adr) : val(inp), head(adr) {};
+        freq_iterator head;
     };
     
-
     struct freq_node{
         int freq;
-        typename std::list<node>::iterator list_node;
-
-        public:
-        freq_node(int inp, typename std::list<node>::iterator adr) : freq(inp), list_node(adr) {};
+        typename std::list<node> list_node;
     };
 
     
@@ -31,61 +25,63 @@ template <typename T> class LFU {
     std::list<freq_node> list_freq;
 
     public:
-    LFU(int size) : sz_max(size) {};
+    LFU(int size) : sz_max(size) {}
 
     bool check_hit(T val) {
         if (hash.find(val) == hash.end()) {
-            if(hash.size() == 0) {
-                list_freq.push_front(freq_node(0, new std::list<node>));
-                list_freq.front().list_node->push_back(node(val, &list_freq.front()));
-                hash.insert(val, &list_freq.front().list_node->back());
+            if(hash.empty()) { 
+                list_freq.emplace_front(0, list_freq.begin());
+                list_freq.front().list_node.emplace_back(val, list_freq.begin());
+                hash.insert(val, std::prev(list_freq.front().list_node.end()));
             }
             else if(hash.size() < sz_max) {
                 if (list_freq.front().freq != 0) {
-                    list_freq.push_front(freq_node(0, new std::list<node>));
+                    list_freq.emplace_front(0, list_freq.begin());
                 }
-                list_freq.front().list_node->push_back(node(val, &list_freq.front()));
-                hash.insert(val, &list_freq.front().list_node->back());
+                list_freq.front().list_node.emplace_back(val, list_freq.begin());
+                hash.insert(val, std::prev(list_freq.front().list_node.end()));
             }
             else {
-                T del_val = list_freq.front().list_node->front().val;
+                T del_val = list_freq.front().list_node.front().val;
                 hash.erase(del_val);
-                list_freq.front().list_node->erase(0);
-                if ((list_freq.front().freq != 0) && (list_freq.front().list_node->size() == 0))
+                list_freq.front().list_node.erase(0);
+                if ((list_freq.front().freq != 0) && (list_freq.front().list_node.size() == 0))
                     list_freq.erase(0);
                 if (list_freq.front().freq != 0)
-                    list_freq.push_front(freq_node(0, new std::list<node>));
-                list_freq.front().list_node->push_back(node(val, &list_freq.front()));
-                hash.insert(val, &list_freq[0].list_node->back());
+                    list_freq.emplace_front(0, list_freq.begin());
+                list_freq.front().list_node.emplace_back(val, list_freq.begin());
+                hash.insert(val, std::prev(list_freq.front().list_node.end()));
             }
             return false;
         }
         else {
-            auto adr = hash.find(val).second;
-            if (adr->head == &list_freq.back()) {
-                list_freq.push_back(freq_node(adr->head->freq + 1, new std::list<node>));
-                list_freq.back().list_node->push_back(node(val, ++(adr->head)));
-                std::advance(list_freq.end(), -2)->node_list->erase(adr);
-                if (std::advance(list_freq.end(), -2)->node_list->size() == 0)
-                    list_freq.erase(std::advance(list_freq.end(), -2));
-                hash.find(val).second = &list_freq.back().list_node->back();
+            auto adr = hash.find(val)->second;
+            if (adr->head == std::prev(list_freq.end())) {
+                list_freq.emplace_back(adr->head->freq + 1, std::prev(list_freq.end()));
+                list_freq.back().list_node.emplace_back(val, std::next(adr->head));
+                auto penult = list_freq.end();
+                std::advance(penult, -2);
+                penult->list_node.erase(adr);
+                if (penult->list_node.size() == 0)
+                    list_freq.erase(penult);
+                hash.find(val)->second = std::prev(list_freq.back().list_node.end());
             }
-            else if (adr->head->freq + 1 == ++(adr->head)->freq) {
-                ++(adr->head)->list_node->push_back(node(val, ++(adr->head)));
-                adr->head->list_node->erase(adr);
-                if (adr->head->list_node->size() == 0)
+            else if (adr->head->freq + 1 == std::next(adr->head)->freq) {
+                std::next(adr->head)->list_node.emplace_back(val, std::next(adr->head));
+                adr->head->list_node.erase(adr);
+                if (adr->head->list_node.size() == 0)
                     list_freq.erase(adr->head);
-                hash.find(val).second = &(++(adr->head)->list_node->back());
+                hash.find(val)->second = std::prev(std::next(adr->head)->list_node.end());
             }
             else {
-                list_freq.insert(++(adr->head), freq_node(adr->head->freq + 1, new std::list<node>));
-                ++(adr->head)->list_node->push_back(node(val, ++(adr->head)));
-                adr->head->list_node->erase(adr);
-                if (adr->head->list_node->size() == 0)
+                list_freq.insert(std::next(adr->head), adr->head->freq + 1, std::next(adr->head));
+                std::next(adr->head)->list_node.emplace_back(val, std::next(adr->head));
+                adr->head->list_node.erase(adr);
+                if (adr->head->list_node.size() == 0)
                     list_freq.erase(adr->head);
-                hash.find(val).second = &(++(adr->head)->list_node->back());    
+                hash.find(val)->second = std::prev(std::next(adr->head)->list_node.end());    
             }
             return true;
         }
-    };
+    }
 };
